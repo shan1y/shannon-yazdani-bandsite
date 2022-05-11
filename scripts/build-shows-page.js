fac3277d3b03e9536c88a650e6c841b3bbf6eb9c
Shannon Yazdani <shannonyazdani@gmail.com>
2022-05-11 17:13:25 -0400

    created form elements

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -1,38 +1,105 @@
-//main "shows" section selector
-const showsSection = document.querySelector(".shows");
+const showsSection = document.querySelector(".shows-info");
 
-//Create Elements
-const showsTitle = document.createElement("h2");
-showsTitle.classList.add("shows__title");
-showsTitle.innerText = "Shows";
+const showsSectionTitle = document.createElement("h2");
+showsSectionTitle.classList.add("shows-info__section-heading");
+showsSectionTitle.textContent = "Shows";
 
-const showsInfoContainer = document.createElement("div");
-showsInfoContainer.classList.add("shows-info__container");
+showsSection.appendChild(showsSectionTitle);
 
-const showsHeading = document.createElement("h1");
-showsHeading.classList.add("shows__title");
-showsHeading.innerHTML = "Shows";
+let information = [
+  {
+    id: "1",
+    date: "Mon Sept 06 2021",
+    venue: "Ronald Lane",
+    location: "San Francisco, CA",
+  },
+  {
+    id: "2",
+    date: "Tue Sept 21 2021",
+    venue: "Pier 3 East",
+    location: "San Francisco, CA",
+  },
+  {
+    id: "3",
+    date: "Fri Oct 15 2021",
+    venue: "View Lounge",
+    location: "San Francisco, CA",
+  },
+  {
+    id: "4",
+    date: "Sat Nov 06 2021",
+    venue: "Hyatt Agency",
+    location: "San Francisco, CA",
+  },
+  {
+    id: "5",
+    date: "Fri Nov 26 2021",
+    venue: "Moscow Center",
+    location: "San Francisco, CA",
+  },
+  {
+    id: "6",
+    date: "Wed Dec 15 2021",
+    venue: "Press Club",
+    location: "San Francisco, CA",
+  },
+];
 
-const showsInfoTitle = document.createElement("p");
-showsInfoTitle.classList.add("shows-info__title");
-showsInfoTitle.innerHTML = "Date";
+information.forEach((item) => {
+  //Create Element Container
+  const showsInfoContainer = document.createElement("ul");
+  showsInfoContainer.classList.add("shows-info__container");
 
-const showsInfoData = document.createElement("p");
-showsInfoData.classList.add("shows-info__data");
-showsInfoData.innerHTML = "Wed Dec 15 2021";
+  //Append container to HTML section
+  showsSection.appendChild(showsInfoContainer);
 
-const showsButton = document.createElement("button");
-showsButton.classList.add("shows__button");
-showsButton.innerHTML = "Buy Tickets";
+  //Create Date Title Element
+  const showsInfoDateHeading = document.createElement("li");
+  showsInfoDateHeading.classList.add("shows-info__heading");
+  showsInfoDateHeading.textContent = "Date";
 
-// <!-- <h1 class="shows__title"></h1>
-// <div class="shows-info__container">
-//     <p class="shows-info__title">date</p>
-//     <p class="shows-info__data">test test test</p>
-// </div> -->
+  //Get Date from Object
+  const showsInfoDate = document.createElement("li");
+  showsInfoDate.classList.add("shows-info__description--date");
+  showsInfoDate.textContent = item.date;
 
-showsSection.appendChild(showsTitle);
-showsSection.appendChild(showsInfoContainer);
-showsInfoContainer.appendChild(showsInfoTitle);
-showsInfoContainer.appendChild(showsInfoData);
-showsInfoContainer.appendChild(showsButton);
+  //Append Date to container
+  showsInfoContainer.appendChild(showsInfoDateHeading);
+  showsInfoContainer.appendChild(showsInfoDate);
+
+  //Create Venue Title Element
+  const showsInfoVenueHeading = document.createElement("li");
+  showsInfoVenueHeading.classList.add("shows-info__heading");
+  showsInfoVenueHeading.textContent = "Venue";
+
+  //Get Venue from Object
+  const showsInfoVenue = document.createElement("li");
+  showsInfoVenue.classList.add("shows-info__description");
+  showsInfoVenue.textContent = item.venue;
+
+  //Append Venue to container
+  showsInfoContainer.appendChild(showsInfoVenueHeading);
+  showsInfoContainer.appendChild(showsInfoVenue);
+
+  //Create Location Title Element
+  const showsInfoLocationHeading = document.createElement("li");
+  showsInfoLocationHeading.classList.add("shows-info__heading");
+  showsInfoLocationHeading.textContent = "Location";
+
+  //Get Location from Object
+  const showsInfoLocation = document.createElement("li");
+  showsInfoLocation.classList.add("shows-info__description");
+  showsInfoLocation.textContent = item.location;
+
+  //Create Button Element
+  const showsInfoButton = document.createElement("button");
+  showsInfoButton.classList.add("shows-info__button");
+  showsInfoButton.textContent = "Buy Tickets";
+
+  //Append Location to container
+  showsInfoContainer.appendChild(showsInfoLocationHeading);
+  showsInfoContainer.appendChild(showsInfoLocation);
+
+  //Append button to container
+  showsInfoContainer.appendChild(showsInfoButton);
+});
---
+++ scripts/index-page.js
@@ -0,0 +1,28 @@
+let commentsSection = [
+  {
+    id: "comment 1",
+    name: "Connor Walton",
+    date: "02/17/2021",
+    comment:
+      "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
+  },
+  {
+    id: "comment 2",
+    name: "Emilie Beach",
+    date: "01/09/2021",
+    comment:
+      "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.",
+  },
+  {
+    id: "comment 3",
+    name: "Miles Acosta",
+    date: "12/20/2020",
+    comment:
+      "I can't stop listening. Every Time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
+  },
+];
+
+
+// function(commentsSection) {
+//   createElement
+// }
